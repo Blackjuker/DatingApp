d000fda58e5912cf6922175318ddc6258a4bf335
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-07 07:36:42 +0200

    simplify api

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4116e4423dad6b6c33e5cca8beb550245d398cd4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4c63eb40a0bacdf792dfec4c92196bfa5c98cab5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
