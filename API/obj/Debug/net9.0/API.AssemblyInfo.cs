82b65914b2581e463836da025f0d7178c42b4f7c
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-07 18:30:05 +0200

    creation de la migration

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be1160b5ff35e6182f4e904f26557bbba9cb8765")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70a3b5b744f68c9380f5748b9ecdd71945d594a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
