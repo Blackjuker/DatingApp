15d3a1577115f75055229d4e026c8981c73a6768
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-10 00:27:21 +0200

    update controller

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,6 +1,8 @@
 using API.Data;
 using API.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {    [Route("api/[controller]")] // localhost:5001/api/members
