c13cb4c72dd9d477ffe1f89d0c2655b96444e99f
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-09 16:56:41 +0200

    update files obj

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -8,24 +8,24 @@ import { lastValueFrom } from 'rxjs';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit{
-  
+export class App implements OnInit {
+
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
   protected members = signal<any>([]);
 
 
-  async ngOnInit(){
+  async ngOnInit() {
     this.members.set(await this.getMembers());
   }
 
 
   async getMembers() {
     try {
-      return lastValueFrom(  this.http.get('https://localhost:5001/api/members'));
-    }catch(error) {
+      return lastValueFrom(this.http.get('https://localhost:5001/api/members'));
+    } catch (error) {
       console.log(error);
       throw error;
+    }
   }
-}
 }
