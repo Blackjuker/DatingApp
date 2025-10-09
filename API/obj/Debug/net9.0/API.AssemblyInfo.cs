5d79f8803535d68405927297a5201dd1465aa8e3
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-09 14:49:34 +0200

    creation du projet client angular

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2d8983a1e56d09e9ba4f1becce36b7ee4e4e5c5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+854e6e1d667761dfa6bf6063a8701aa364f3b809")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
