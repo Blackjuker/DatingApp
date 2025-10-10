a211a81841931f23397365ef9023bddbbe38f941
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-10 16:05:45 +0200

    service de creation de token

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e207c1095396dcdc7b1745d5f94c1486ee117a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ec07bccceb9073fe2b506ae5246c7c215b6bc8c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
