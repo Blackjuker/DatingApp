ac7fce1080adf867f10046b3991a444c2490bfc0
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-11 13:50:39 +0200

    register and retour au home

--- client/src/core/services/account-service.ts
+++ client/src/core/services/account-service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable, signal } from '@angular/core';
-import { User } from '../../types/user';
+import { LoginCreds, RegisterCreds, User } from '../../types/user';
 import { tap } from 'rxjs';
 
 @Injectable({
@@ -11,18 +11,32 @@ export class AccountService {
   currentUser = signal<User | null>(null);
   baseUrl = 'https://localhost:5001/api/';
 
-  login(creds:any){
-    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
-      tap(user =>{
-        if(user){
-          localStorage.setItem('user', JSON.stringify(user));
-          this.currentUser.set(user);
+  register(creds: RegisterCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/register', creds).pipe(
+      tap(user => {
+        if (user) {
+          this.setCurrentUser(user);
         }
       })
     );
   }
 
-  logout(){
+  login(creds: LoginCreds) {
+    return this.http.post<User>(this.baseUrl + 'account/login', creds).pipe(
+      tap(user => {
+        if (user) {
+          this.setCurrentUser(user);
+        }
+      })
+    );
+  }
+
+  setCurrentUser(user: User) {
+    localStorage.setItem('user', JSON.stringify(user));
+    this.currentUser.set(user);
+  }
+
+  logout() {
     this.currentUser.set(null);
     localStorage.removeItem('user');
   }
