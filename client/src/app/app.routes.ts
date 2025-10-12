2322fd2599d5f0e64b6b99802d8e8a723958de53
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-13 00:39:04 +0200

    add toast services

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,9 +2,14 @@ import { Routes } from '@angular/router';
 import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
 import { MemberList } from '../features/members/member-list/member-list';
 import { Home } from '../features/home/home';
+import { Lists } from '../features/lists/lists';
+import { Messages } from '../features/messages/messages';
 
 export const routes: Routes = [
     {path:'',component:Home},
     {path:'members',component:MemberList},
     {path:'members/:id',component:MemberDetailed},
+    {path:'list',component:Lists},
+    {path:'messages',component:Messages},
+    {path:'**',redirectTo:'',pathMatch:'full'},
 ];
