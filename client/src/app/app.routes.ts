1def64f9edef9eb369dd10c6b177feb0a2560f58
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-12 18:46:44 +0200

    add components in projet

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { MemberDetailed } from '../features/members/member-detailed/member-detailed';
+import { MemberList } from '../features/members/member-list/member-list';
+import { Home } from '../features/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:Home},
+    {path:'members',component:MemberList},
+    {path:'members/:id',component:MemberDetailed},
+];
