49cdbcec118d240f73d2ff3b1baf47a744605785
Blackjuker <ngabgnaarnaud2@gmail.com>
2025-10-27 17:33:36 +0100

    change de ngForm à reactif Form Group avec un mock up pour voir le code en direct

--- client/src/features/account/register/register.ts
+++ client/src/features/account/register/register.ts
@@ -1,30 +1,48 @@
-import { Component, inject, input, output } from '@angular/core';
-import { RegisterCreds, User } from '../../../types/user';
-import { FormsModule } from '@angular/forms';
+import { Component, inject, OnInit, output } from '@angular/core';
+import { RegisterCreds } from '../../../types/user';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../../../core/services/account-service';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [FormsModule],
+  imports: [ReactiveFormsModule,JsonPipe],
   templateUrl: './register.html',
   styleUrl: './register.css'
 })
-export class Register {
+export class Register implements OnInit {
+  
    //membersFromHome = input.required<User[]>(); // laisser sans protected pour que cela soit public pour recuerer du parent
    private accountService = inject(AccountService);
    cancelRegister = output<boolean>();
   protected creds = {} as RegisterCreds;
+  protected registerForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.initializeForm();
+  }
+
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      // définir les contrôles du formulaire ici
+      email: new FormControl(),
+      displayName: new FormControl(),
+      password: new FormControl(),
+      confirmPassword: new FormControl()
+    });
+  }
 
   register(){
-    this.accountService.register(this.creds).subscribe({
-      next: response => {
-        console.log(response);
-        this.cancel();
-      },
-      error: error => {
-        console.log(error);
-      }
-    }); // ajouter gestion erreur
+    console.log(this.registerForm.value);
+    // this.accountService.register(this.creds).subscribe({
+    //   next: response => {
+    //     console.log(response);
+    //     this.cancel();
+    //   },
+    //   error: error => {
+    //     console.log(error);
+    //   }
+    // }); // ajouter gestion erreur
   }
 
   cancel(){
